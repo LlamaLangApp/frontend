cfe227e0fad5a4b241938b41997831d9cf8dd15b
Alicja Hurbol <alicja.hurbol@gmail.com>
2023-07-10 15:29:10 +0200

    fix

--- games/race/RaceGame.tsx
+++ games/race/RaceGame.tsx
@@ -1,9 +1,9 @@
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { RaceStackParamList } from "./RaceStack";
+// import { NativeStackScreenProps } from "@react-navigation/native-stack";
+// import { RaceStackParamList } from "./RaceStack";
 import { Text, View } from "react-native";
 import mainStyles from "../../styles/MainStyles";
 
-type Props = NativeStackScreenProps<RaceStackParamList, "Game">;
+// type Props = NativeStackScreenProps<RaceStackParamList, "Game">;
 
 function RaceGameScreen() {
   return (
